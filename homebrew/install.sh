1699506ddbdeabb8efe9160c9a0c8c80a5112756
Chad Metcalf <metcalfc@gmail.com>
2022-03-09 01:09:28 +0000

    Install brew on Gitpod without real root

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,12 +9,24 @@ source "$(dirname $0)"/../script/bashpodlib.sh
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
-  /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
+  if [ "${GITPOD_REPO_ROOT}" == "/workspace/dotfiles"]; then
 
-  check_brew_command powerline-go
+    echo "  Installing Homebrew in Gitpod for you to test."
+    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+    mkdir ~/.linuxbrew/bin
+    ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+    eval $(~/.linuxbrew/bin/brew shellenv)
+
+  else
+
+    echo "  Installing Homebrew for you."
+    /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  
+  fi
+  
 fi
+
+check_brew_command powerline-go
 
 success "Installed Homebrew Things"
 exit 0
