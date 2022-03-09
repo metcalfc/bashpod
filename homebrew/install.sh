524adb8dbc9754faf1de6adf58e102f817247baa
Chad Metcalf <metcalfc@gmail.com>
2022-03-09 03:49:15 +0000

    Just use Gitpod brew.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,18 +9,9 @@ source "$(dirname $0)"/../script/bashpodlib.sh
 # Check for Homebrew
 if test ! $(which brew)
 then
-  if [ "${GITPOD_REPO_ROOT}" == "/workspace/dotfiles" ]; then
 
-    echo "  Installing Homebrew in Gitpod for you to test."
-    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-    mkdir ~/.linuxbrew/bin
-    ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
-    eval $(~/.linuxbrew/bin/brew shellenv)
-
-  else
-
-    echo "  Installing Homebrew for you."
-    /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "  Installing Homebrew for you."
+  /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   
   fi
   
